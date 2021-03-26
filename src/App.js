cfa58084743f0e1f779979500ac0385d235cef1c
Mike Fierro <mfierro31@yahoo.com>
2021-03-26 16:28:16 -0700

    created a ScrollToTop component that makes sure on page change, the page always starts at top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Routes from './Routes';
 import NavBar from './NavBar';
+import ScrollToTop from './ScrollTotop';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <NavBar />
       <Routes />
     </div>
